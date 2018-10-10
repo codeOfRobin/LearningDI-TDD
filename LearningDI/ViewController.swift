e43054eb5c7d6a6e57f0bcf5fa66eddbf6636bfe
Robin Malhotra <me@rmalhotra.com>
2018-10-10 15:41:08 +0530

    All tests now passing. Added a dataSource callback
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- LearningDI/ViewController.swift
+++ LearningDI/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController {
     }
 
     func fetchMinions(minionService: MinionService = MinionService()) {
-        minionService.getTheMinions { (minions) in
+        minionService.getTheMinions { [weak self] (minions) in
+            self?.dataSource = minions
         }
     }
 
